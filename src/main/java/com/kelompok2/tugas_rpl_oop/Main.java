6851b56bc9ed0893f48cf0a20e3f5ccf13cfd06d
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2020-12-03 19:08:38 +0700

    Update Main.java
    Make Main to be more interactive by providing menu to insert new
    User, Professor, and Student.

--- src/main/java/com/kelompok2/tugas_rpl_oop/Main.java
+++ src/main/java/com/kelompok2/tugas_rpl_oop/Main.java
@@ -9,49 +9,129 @@ import java.util.*;
  * @author R. Komang Eling Pramuatmaja (185150200111081)
  */
 public class Main {
+
     public static void main(String[] args) {
-        Professor user1 = new Professor();
-        user1.setName("Fernandes");
-        user1.setEmail("Fernandes@gmail.com");
-        user1.setPassword("Password");
-        user1.setAddress("Jakarta");
-        user1.setContactNumber("08135552123");
-        user1.setStaffId(21113);
-        user1.setStaffId(02112);
-        user1.setSalary(2000);
-        user1.setDepartment("Technology");
+        Scanner in = new Scanner(System.in);
+        List<User> daftarUser = new LinkedList<>();
+        List<Professor> daftarProfessor = new LinkedList<>();
+        List<Student> daftarStudent = new LinkedList<>();
+        String name, password, email, address, contactNumber, department;
+        int staffId, stdId;
+        float salary;
+        boolean isContinue = true;
 
-        Student user2 = new Student();
-        user2.setName("Cia");
-        user2.setEmail("cia@gmail.com");
-        user2.setPassword("Password1");
-        user2.setAddress("Jakarta");
-        user2.setContactNumber("08135552124");
-        user2.setStdId(11112);
+        while (isContinue) {
+            System.out.println("==MENU==");
+            System.out.println("1. Tambah User");
+            System.out.println("2. Tambah Profesor");
+            System.out.println("3. Tambah Siswa");
+            System.out.println("4. Lihat User");
+            System.out.println("5. Lihat Profesor");
+            System.out.println("6. Lihat Siswa");
+            System.out.println("0. Keluar");
+            System.out.print("Pilihan: ");
+            String input = in.nextLine();
+            System.out.println();
 
-        Student user3 = new Student();
-        user3.setName("Nathalie");
-        user3.setEmail("nathalie@gmail.com");
-        user3.setPassword("Password2");
-        user3.setAddress("Jakarta");
-        user3.setContactNumber("08135552125");
-        user3.setStdId(11113);
-
-        Student user4 = new Student();
-        user4.setName("John");
-        user4.setEmail("john@gmail.com");
-        user4.setPassword("Password3");
-        user4.setAddress("Baubau");
-        user4.setContactNumber("08135552126");
-        user4.setStdId(11114);
-
-        System.out.println("Halo dunia!");
-        System.out.println(user1.getName());
-        System.out.println(user1.getEmail());
-        System.out.println(user1.getPassword());
-        System.out.println(user1.getAddress());
-        System.out.println(user1.getContactNumber());
-        System.out.println(user1.getStaffId());
+            switch (input) {
+                case "1":
+                    System.out.println("Tambah User");
+                    System.out.print("Nama: ");
+                    name = in.nextLine();
+                    System.out.print("Password: ");
+                    password = in.nextLine();
+                    System.out.print("Email: ");
+                    email = in.nextLine();
+                    System.out.print("Alamat: ");
+                    address = in.nextLine();
+                    System.out.print("Telepon: ");
+                    contactNumber = in.nextLine();
+                    User user = new User(name, password, email, address, contactNumber);
+                    daftarUser.add(user);
+                    System.out.println("Penambahan User " + name + " berhasil!\n");
+                    break;
+                case "2":
+                    System.out.println("Tambah Profesor");
+                    System.out.print("Nama: ");
+                    name = in.nextLine();
+                    System.out.print("Password: ");
+                    password = in.nextLine();
+                    System.out.print("Email: ");
+                    email = in.nextLine();
+                    System.out.print("Alamat: ");
+                    address = in.nextLine();
+                    System.out.print("Telepon: ");
+                    contactNumber = in.nextLine();
+                    System.out.print("ID Staf: ");
+                    staffId = Integer.parseInt(in.nextLine());
+                    System.out.print("Gaji: ");
+                    salary = Float.parseFloat(in.nextLine());
+                    System.out.print("Departemen: ");
+                    department = in.nextLine();
+                    Professor professor = new Professor(name, password, email, address, contactNumber, staffId, salary, department);
+                    daftarProfessor.add(professor);
+                    System.out.println("Penambahan Profesor " + name + " berhasil!\n");
+                    break;
+                case "3":
+                    System.out.println("Tambah Siswa");
+                    System.out.print("Nama: ");
+                    name = in.nextLine();
+                    System.out.print("Password: ");
+                    password = in.nextLine();
+                    System.out.print("Email: ");
+                    email = in.nextLine();
+                    System.out.print("Alamat: ");
+                    address = in.nextLine();
+                    System.out.print("Telepon: ");
+                    contactNumber = in.nextLine();
+                    System.out.print("ID Siswa: ");
+                    stdId = Integer.parseInt(in.nextLine());
+                    Student student = new Student(name, password, email, address, contactNumber, stdId);
+                    daftarStudent.add(student);
+                    System.out.println("Penambahan Siswa " + name + " berhasil!\n");
+                    break;
+                case "4":
+                    System.out.println("Daftar User");
+                    for (User e : daftarUser) {
+                        System.out.printf("Nama:\t\t %s\n", e.getName());
+                        System.out.printf("Password:\t %s\n", e.getPassword());
+                        System.out.printf("Email:\t\t %s\n", e.getEmail());
+                        System.out.printf("Alamat:\t\t %s\n", e.getAddress());
+                        System.out.printf("Telepon:\t %s\n", e.getContactNumber());
+                        System.out.println();
+                    }
+                    break;
+                case "5":
+                    System.out.println("Daftar Profesor");
+                    for (Professor e : daftarProfessor) {
+                        System.out.printf("Nama:\t\t %s\n", e.getName());
+                        System.out.printf("Password:\t %s\n", e.getPassword());
+                        System.out.printf("Email:\t\t %s\n", e.getEmail());
+                        System.out.printf("Alamat:\t\t %s\n", e.getAddress());
+                        System.out.printf("Telepon:\t %s\n", e.getContactNumber());
+                        System.out.printf("ID Staf:\t %s\n", e.getStaffId());
+                        System.out.printf("Gaji:\t\t %s\n", e.getSalary());
+                        System.out.printf("Departemen:\t %s\n", e.getDepartment());
+                        System.out.println();
+                    }
+                    break;
+                case "6":
+                    System.out.println("Daftar Siswa");
+                    for (Student e : daftarStudent) {
+                        System.out.printf("Nama:\t\t %s\n", e.getName());
+                        System.out.printf("Password:\t %s\n", e.getPassword());
+                        System.out.printf("Email:\t\t %s\n", e.getEmail());
+                        System.out.printf("Alamat:\t\t %s\n", e.getAddress());
+                        System.out.printf("Telepon:\t %s\n", e.getContactNumber());
+                        System.out.printf("ID Siswa:\t %s\n", e.getStdId());
+                        System.out.println();
+                    }
+                    break;
+                default:
+                    isContinue = false;
+                    System.out.println("Keluar");
+            }
+        }
     }
 
 }
