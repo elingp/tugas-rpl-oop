e0c2faabf83c8b42b16a8cdc04f977dcf2cf8696
marveltimothy <marvel.manullang10@gmail.com>
2020-12-03 20:40:09 +0800

    Update main
    Menghapus user dan menambahkan interaksi

--- src/main/java/com/kelompok2/tugas_rpl_oop/Main.java
+++ src/main/java/com/kelompok2/tugas_rpl_oop/Main.java
@@ -12,7 +12,6 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        List<User> daftarUser = new LinkedList<>();
         List<Professor> daftarProfessor = new LinkedList<>();
         List<Student> daftarStudent = new LinkedList<>();
         String name, password, email, address, contactNumber, department;
@@ -22,12 +21,10 @@ public class Main {
 
         while (isContinue) {
             System.out.println("==MENU==");
-            System.out.println("1. Tambah User");
-            System.out.println("2. Tambah Profesor");
-            System.out.println("3. Tambah Siswa");
-            System.out.println("4. Lihat User");
-            System.out.println("5. Lihat Profesor");
-            System.out.println("6. Lihat Siswa");
+            System.out.println("1. Tambah Profesor");
+            System.out.println("2. Tambah Siswa");
+            System.out.println("3. Lihat Profesor");
+            System.out.println("4. Lihat Siswa");
             System.out.println("0. Keluar");
             System.out.print("Pilihan: ");
             String input = in.nextLine();
@@ -35,22 +32,6 @@ public class Main {
 
             switch (input) {
                 case "1":
-                    System.out.println("Tambah User");
-                    System.out.print("Nama: ");
-                    name = in.nextLine();
-                    System.out.print("Password: ");
-                    password = in.nextLine();
-                    System.out.print("Email: ");
-                    email = in.nextLine();
-                    System.out.print("Alamat: ");
-                    address = in.nextLine();
-                    System.out.print("Telepon: ");
-                    contactNumber = in.nextLine();
-                    User user = new User(name, password, email, address, contactNumber);
-                    daftarUser.add(user);
-                    System.out.println("Penambahan User " + name + " berhasil!\n");
-                    break;
-                case "2":
                     System.out.println("Tambah Profesor");
                     System.out.print("Nama: ");
                     name = in.nextLine();
@@ -72,7 +53,7 @@ public class Main {
                     daftarProfessor.add(professor);
                     System.out.println("Penambahan Profesor " + name + " berhasil!\n");
                     break;
-                case "3":
+                case "2":
                     System.out.println("Tambah Siswa");
                     System.out.print("Nama: ");
                     name = in.nextLine();
@@ -90,41 +71,42 @@ public class Main {
                     daftarStudent.add(student);
                     System.out.println("Penambahan Siswa " + name + " berhasil!\n");
                     break;
-                case "4":
-                    System.out.println("Daftar User");
-                    for (User e : daftarUser) {
-                        System.out.printf("Nama:\t\t %s\n", e.getName());
-                        System.out.printf("Password:\t %s\n", e.getPassword());
-                        System.out.printf("Email:\t\t %s\n", e.getEmail());
-                        System.out.printf("Alamat:\t\t %s\n", e.getAddress());
-                        System.out.printf("Telepon:\t %s\n", e.getContactNumber());
-                        System.out.println();
-                    }
-                    break;
-                case "5":
+                case "3":
                     System.out.println("Daftar Profesor");
-                    for (Professor e : daftarProfessor) {
-                        System.out.printf("Nama:\t\t %s\n", e.getName());
-                        System.out.printf("Password:\t %s\n", e.getPassword());
-                        System.out.printf("Email:\t\t %s\n", e.getEmail());
-                        System.out.printf("Alamat:\t\t %s\n", e.getAddress());
-                        System.out.printf("Telepon:\t %s\n", e.getContactNumber());
-                        System.out.printf("ID Staf:\t %s\n", e.getStaffId());
-                        System.out.printf("Gaji:\t\t %s\n", e.getSalary());
-                        System.out.printf("Departemen:\t %s\n", e.getDepartment());
-                        System.out.println();
+                    if (!daftarProfessor.isEmpty()) {
+                        for (Professor e : daftarProfessor) {
+                            System.out.printf("Nama:\t\t %s\n", e.getName());
+                            System.out.printf("Password:\t %s\n", e.getPassword());
+                            System.out.printf("Email:\t\t %s\n", e.getEmail());
+                            System.out.printf("Alamat:\t\t %s\n", e.getAddress());
+                            System.out.printf("Telepon:\t %s\n", e.getContactNumber());
+                            System.out.printf("ID Staf:\t %s\n", e.getStaffId());
+                            System.out.printf("Gaji:\t\t %s\n", e.getSalary());
+                            System.out.printf("Departemen:\t %s\n", e.getDepartment());
+                            System.out.println();
+                        }
+                    } else {
+                        System.out.println("");
+                        System.out.println("Tidak Ada Data!");
+                        System.out.println("");
                     }
                     break;
-                case "6":
+                case "4":
                     System.out.println("Daftar Siswa");
-                    for (Student e : daftarStudent) {
-                        System.out.printf("Nama:\t\t %s\n", e.getName());
-                        System.out.printf("Password:\t %s\n", e.getPassword());
-                        System.out.printf("Email:\t\t %s\n", e.getEmail());
-                        System.out.printf("Alamat:\t\t %s\n", e.getAddress());
-                        System.out.printf("Telepon:\t %s\n", e.getContactNumber());
-                        System.out.printf("ID Siswa:\t %s\n", e.getStdId());
-                        System.out.println();
+                    if (!daftarProfessor.isEmpty()) {
+                        for (Student e : daftarStudent) {
+                            System.out.printf("Nama:\t\t %s\n", e.getName());
+                            System.out.printf("Password:\t %s\n", e.getPassword());
+                            System.out.printf("Email:\t\t %s\n", e.getEmail());
+                            System.out.printf("Alamat:\t\t %s\n", e.getAddress());
+                            System.out.printf("Telepon:\t %s\n", e.getContactNumber());
+                            System.out.printf("ID Siswa:\t %s\n", e.getStdId());
+                            System.out.println();
+                        }
+                    } else {
+                        System.out.println("");
+                        System.out.println("Tidak Ada Data!");
+                        System.out.println("");
                     }
                     break;
                 default:
